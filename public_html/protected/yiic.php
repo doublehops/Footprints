b47654f73c1e33d929981479b2178eeb57d8cf3b
Damien Buttler <damien@doublehops.com>
2010-06-29 06:04:42 +0000

    Initial commit

---
+++ public_html/protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../yii_framework/yii-1.1.2.r2086/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
