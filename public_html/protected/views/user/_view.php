1924ceafe5fdf064104323370a65113e70116b40
Damien Buttler <damien@doublehops.com>
2010-07-01 03:10:00 +0000

    Added User and UserExtended MVC.

---
+++ public_html/protected/views/user/_view.php
@@ -0,0 +1,32 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
+	<?php echo CHtml::encode($data->username); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('profile')); ?>:</b>
+	<?php echo CHtml::encode($data->profile); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('active')); ?>:</b>
+	<?php echo CHtml::encode($data->active); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('lastModified')); ?>:</b>
+	<?php echo CHtml::encode($data->lastModified); ?>
+	<br />
+
+
+</div>
