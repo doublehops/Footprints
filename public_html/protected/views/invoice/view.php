b50e9b4bbd09ee395b249758ff4bfe5c1364a430
Damien Buttler <damien@doublehops.com>
2010-08-17 12:09:51 +0000

    Added invoiceView link to Invoice view menu.

--- public_html/protected/views/invoice/view.php
+++ public_html/protected/views/invoice/view.php
@@ -10,6 +10,7 @@ $this->menu=array(
 	array('label'=>'Update Invoice', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Delete Invoice', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Invoice', 'url'=>array('admin')),
+	array('label'=>'Extended View', 'url'=>array('invoiceView', 'id'=>$model->id)),
 );
 ?>
 
