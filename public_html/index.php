79f9958108ec1b281375f72e5f11cc2ac7c026bc
Damien Buttler <damien@doublehops.com>
2010-09-01 01:38:24 +0000

    Changed index file to accommodate multiple hosts.

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,32 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../yii_framework/yii-1.1.2.r2086/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+switch( $_SERVER['SERVER_NAME'] ) 
+{
+	case 'footprints' :
+		 
+		$yii=dirname(__FILE__).'/../../../yii_framework/yii-1.1.2.r2086/framework/yii.php';
+		$config=dirname(__FILE__).'/protected/config/main.php';
+		
+		// remove the following lines when in production mode
+		defined('YII_DEBUG') or define('YII_DEBUG',true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		
+		break;
+		
+	case 'footprints.doublehops.com' :
+		
+		$yii=dirname( '/var/www/yii_framework/yii-1.1.2.r2086/framework/yii.php');
+		$config=dirname(__FILE__).'/protected/config/main.php';
+		
+		// remove the following lines when in production mode
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		
+		
+		break;
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
