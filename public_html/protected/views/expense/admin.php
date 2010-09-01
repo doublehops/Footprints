2f4173ca61d565f6979ad585a62d431f93e78e07
Damien Buttler <damien@doublehops.com>
2010-09-01 10:24:51 +0000

    Removed expense description from expense admin view.

--- public_html/protected/views/expense/admin.php
+++ public_html/protected/views/expense/admin.php
@@ -45,7 +45,6 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'id',
 		'creditor.name',
 		'expenseName',
-		'expenseDescription',
 		'expenseTotal',
 		'expensePaid',
 		/*
