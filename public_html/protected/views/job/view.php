98e01ffdfbb5cb5bcc996f60477e1962c7bd8ac9
Damien Buttler <damien@doublehops.com>
2012-03-29 12:49:58 +1100

    Clicking the Add New Job link now sends invoice id filter.

--- public_html/protected/views/job/view.php
+++ public_html/protected/views/job/view.php
@@ -8,6 +8,7 @@ $this->menu=array(
 	array('label'=>'List Job', 'url'=>array('index')),
 	array('label'=>'Create Job', 'url'=>array('create')),
 	array('label'=>'Update Job', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Create Job', 'url'=>array('job/create', 'iid'=>$model->invoice->id)),
 	array('label'=>'Delete Job', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Job', 'url'=>array('admin')),
 	array('label'=>'Job Invoice', 'url'=>array('invoice/view', 'id'=>$model->invoice->id)),
