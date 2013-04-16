df88d6217ba6eae1fb7a24f12e4f2fe82e8a03e5
Damien Buttler <damien@doublehops.com>
2013-04-16 10:55:14 +1000

    Allowing any logged in user to use actions in Expense.

--- protected/controllers/ExpenseController.php
+++ protected/controllers/ExpenseController.php
@@ -31,16 +31,12 @@ class ExpenseController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','calculateExpenses'),
+				'actions'=>array('admin','index','view','create','update','calculateExpenses'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('delete'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
